6236c9f36c0a6415cd8a1a1f110914b6b4b1b599
Florian <info@codinginflow.com>
2021-05-20 09:03:16 +0200

    End of part 2

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector3(0, 14, 0);
+        }
+    }
+}

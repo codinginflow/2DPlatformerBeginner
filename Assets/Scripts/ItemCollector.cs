c37320622358f8082390b5a223302cf5c3c4cf14
Florian <info@codinginflow.com>
2021-05-21 10:05:18 +0200

    End of part 7

---
+++ Assets/Scripts/ItemCollector.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemCollector : MonoBehaviour
+{
+    private int cherries = 0;
+
+    [SerializeField] private Text cherriesText;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Cherry"))
+        {
+            Destroy(collision.gameObject);
+            cherries++;
+            cherriesText.text = "Cherries: " + cherries;
+        }
+    }
+}

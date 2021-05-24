deceeeebc78eb1ce676ea4546e1e65febefcb183
Florian <info@codinginflow.com>
2021-05-24 11:46:18 +0200

    End of part 12

--- Assets/Scripts/Finish.cs
+++ Assets/Scripts/Finish.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Finish : MonoBehaviour
 {
     private AudioSource finishSound;
+
+    private bool levelCompleted = false;
 
     private void Start()
     {
@@ -13,15 +16,16 @@ public class Finish : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.name == "Player")
+        if (collision.gameObject.name == "Player" && !levelCompleted)
         {
             finishSound.Play();
-            CompleteLevel();
+            levelCompleted = true;
+            Invoke("CompleteLevel", 2f);
         }
     }
 
     private void CompleteLevel()
     {
-
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
